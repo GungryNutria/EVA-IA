c72b4e4d2284fab08f569da65caeba27fad884f8
GungryNutria <edgardochamorro08@gmail.com>
2022-10-02 16:44:00 -0500

    Propuesta de codigo

--- main.py
+++ main.py
@@ -106,44 +106,43 @@ def run() -> None:
                     tensor_image = vision.TensorImage.create_from_array(rgb_image)
                     # List classification results
                     categories = classifier.classify(tensor_image)
-                                    
-                    for idx, category in enumerate(categories.classifications[0].categories):
+                    
+                    material = max(categories.classifications[0].categories, key=categories.classifications[0].categories.get)
+                    max_score = max(categories.classifications[0].categories)
 
-                        score = round(category.score, 2) * 100
-
-                        # if category.category_name == 'aluminio' and score >= 10:
-                        #     cv2.imwrite(saveImage('aluminio'),image)
-                        #     aluminio+=1
-                        #     procesos.append(65)
-                        #     # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
-                        #     print('{} {}: {}%'.format(category.category_name,aluminio,score))
-                        #     break
-                        # elif category.category_name == 'plastico' and score >= 10:
-                        #     cv2.imwrite(saveImage('plastico'),image)
-                        #     plastico+=1
-                        #     procesos.append(72)
-                        #     # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
-                        #     print('{} {}: {}%'.format(category.category_name,plastico,score))
-                        #     break
-                        # elif category.category_name == 'hojalata' and score >= 10:
-                        #     cv2.imwrite(saveImage('hojalata'),image)
-                        #     hojalata+=1
-                        #     procesos.append(80)
-                        #     # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
-                        #     print('{} {}: {}%'.format(category.category_name,hojalata,score))
-                        #     break
-                        # elif category.category_name == 'fondo' and score >= 50:
-                        #     fondo+=1
-                        #     procesos.append(72)
-                        #     # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
-                        #     print(category.category_name + ': ' + str(hojalata)+': '+ str(score) +'%')
-                        #     break
-                        #  else:
-                        #     cv2.imwrite(saveImage('desconocido'),image)
-                        #     desconocido+=1
-                        #     procesos.append(68)
-                        #     print('desconocido: '+str(desconocido))
-                        #     break
+                    if material == 'aluminio' and max_score >= 10:
+                        cv2.imwrite(saveImage('aluminio'),image)
+                        aluminio+=1
+                        procesos.append(65)
+                        # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
+                        print('{} {}: {}%'.format(material,aluminio,max_score))
+                        break
+                    elif material == 'plastico' and max_score >= 10:
+                        cv2.imwrite(saveImage('plastico'),image)
+                        plastico+=1
+                        procesos.append(72)
+                        # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
+                        print('{} {}: {}%'.format(material,plastico,max_score))
+                        break
+                    elif material == 'hojalata' and max_score >= 10:
+                        cv2.imwrite(saveImage('hojalata'),image)
+                        hojalata+=1
+                        procesos.append(80)
+                        # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
+                        print('{} {}: {}%'.format(material,hojalata,max_score))
+                        break
+                    elif material == 'fondo' and max_score >= 50:
+                        fondo+=1
+                        procesos.append(72)
+                        # esp_nextion.write(respuesta.encode(encoding='UTF-8',errors='strict'))
+                        print(material + ': ' + str(hojalata)+': '+ str(max_score) +'%')
+                        break
+                    else:
+                        cv2.imwrite(saveImage('desconocido'),image)
+                        desconocido+=1
+                        procesos.append(68)
+                        print('desconocido: '+str(desconocido))
+                        break
 
                     if IA_STATUS_OFF:
                         IA_STATUS_ON = False
